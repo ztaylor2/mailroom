fcb1e8fa020b07434ebe6452866f3ae1f2d1fbcc
Michael Shinners <mishinners@icloud.com>
2017-10-20 21:56:26 -0700

    Create table function nearly operational. Just need one final step to complete. Also did some prelim work on the send_thank_you function.

--- mailroom.py
+++ mailroom.py
@@ -3,7 +3,8 @@
 import sys
 
 
-USER_DONATION_HISTORY = {"zach": [1, 2, 3], "mike": [5000, 1, 3], "someone": [5, 2, 3]}
+USER_DONATION_HISTORY = {"zach": [1, 2, 3], "mike": [5000, 1, 3], "someone":
+                         [5, 2, 3]}
 
 
 def get_user_input(prompt, validator=None):
@@ -28,6 +29,7 @@ def validate_user_input_thank_report(reply):
 
 def send_a_thank_you():
     """."""
+
     print('thank you was put in')
 
 
@@ -40,7 +42,25 @@ def create_a_report():
         new_donor.append(key)
         donors_donation_list.append(new_donor)
     print(sorted(donors_donation_list, reverse=True))
+    print(create_donor_table(USER_DONATION_HISTORY, colList=None))
     main()
+
+
+def create_donor_table(USER_DONATION_HISTORY, colList=None):
+    """Function puts our donor data in a table and prints it out."""
+    # if not column_list:  # This line throws an error that I cant figure.
+    column_list = list(USER_DONATION_HISTORY[0].keys()
+                       if USER_DONATION_HISTORY
+                       else []
+                       )
+    my_list = [column_list]  # 1st row = header
+    for donor in USER_DONATION_HISTORY:
+        my_list.append([str(donor[col] or '') for col in column_list])
+    column_size = [max(map(len, col)) for col in zip(*my_list)]
+    table_row = ' | '.join(["{{:<{}}}".format(i) for i in column_size])
+    my_list.insert(1, ['-' * i for i in column_size])  # Seperating line
+    for donor in my_list:
+        print(table_row.format(*donor))
 
 
 def main():
